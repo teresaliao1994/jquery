3d451b36ab8e4e3af6fb3acb3c161700d4f4c9fd
Pinjung Liao <pinjung.liao@gmail.com>
2022-05-07 15:04:52 +0800

    新增all.css scss

--- js/all.js
+++ js/all.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+  // dropdown
+  $('.menu__link').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active').parent().siblings().find('.menu__link').removeClass('active');
+    $(this).siblings('.dropdown').slideToggle();
+    $(this).parent().siblings().find('.dropdown').slideUp();
+    onclick
+    $(selector).on(events, function () {
+
+    });
+  });
+
+  // scrollTop
+  $('.top a').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 700);
+  });
+
   //swiper效果
   const swiper = new Swiper('.swiper', {
     loop: true,
@@ -19,5 +39,4 @@ $(document).ready(function () {
     'resizeDuration': 200,
     'wrapAround': true
   })
-
 });
